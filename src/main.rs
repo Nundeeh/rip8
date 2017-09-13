245a3f7eec02dd19ecd3cdccdfa9ff970ecfa81f
Nundeeh <MichelMoo@web.de>
2017-09-13 19:30:51 +0200

    takes filename as parameter and reads file in most ugly way

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs::File;
+use std::io::prelude::*;
 
 struct Chip8 {
   memory: [u8; 4096], 
@@ -12,9 +13,9 @@ struct Chip8 {
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-  let mut f=File::open(filename)?;
+  let filename = &args[1]; // 0 is the name of the program
+  let mut f=File::open(filename).expect("Error while opening the file.");
   let mut content = String::new();
-  f.read_to_string(&mut content)?;
-  for line in search(&config.query, &content) {
-     println!("{}", line); 
+  f.read_to_string(&mut content).expect("Error while reading the file.");
 }
+
