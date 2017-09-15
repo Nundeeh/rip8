4a3250337b3625d6c1e3a3cb04e71cc0fe363a8f
Nundeeh <MichelMoo@web.de>
2017-09-15 21:03:43 +0200

    fetches opcode and implemented 0x2NNN

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,26 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-struct Chip8 {
+ struct Chip8 {
   memory: [u8; 4096], 
   register: [u8; 16],
   index: u16,
   pc: u16,
   display: [u8; 30*64],
   stack: [u16; 16],
-  sp: u16
+  sp: u16,
+  opcode: u16
 }
 
 impl Chip8 {
-  fn new(op_code: Vec<u8>) -> Chip8 {
+  fn new(op_code: Vec<u8>) ->  Chip8 {
     let mut memory: [u8; 4096] = [0; 4096];
 
     for (i, byte) in op_code.iter().enumerate() {
       memory[0x200 + i] = byte.clone();
     }
 
-    Chip8 {
+     Chip8 {
       memory,
       register: [0; 16],
       index: 0, 
@@ -28,25 +29,47 @@ impl Chip8 {
       display: [0; 30*64],
       stack: [0; 16],
       sp: 0,
+      opcode: 0
    }
   }
+
+  fn run_cycle(&mut self) {
+    self.fetch_opcode();
+    self.run_opcode();
+  }
+
+  fn fetch_opcode(&mut self) {
+    self.opcode = (self.memory[self.pc as usize] as u16) << 8 
+    | self.memory[(self.pc + 1) as usize] as u16;
+  }
+
+  fn run_opcode(&mut self) {
+    match self.opcode & 0xF000 {
+     0x2000 =>  {
+      //2NNN: Jump to NNN 
+      self.stack[self.sp as usize] = self.pc;
+      self.sp += 1;
+      self.pc = self.opcode & 0x0FFF;
+     }
+
+     _ => println!("opcode: {:X},not implemented yet", self.opcode)
+
+    }
+  }
+}
+
+fn read_rom(path: String) -> Vec<u8> {
+  let mut f = File::open(&path).expect("Error while opening file.");
+  let mut rom = Vec::new();
+  f.read_to_end(&mut rom).expect("Error while reading file.");
+  rom
 }
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  let path = &args[1]; // 0 is the name of the program
+  let path: String = env::args().nth(1).unwrap(); 
+  let rom = read_rom(path);
 
-  let mut f = File::open(&path).expect("Error while opening file.");
-  let mut op_code = Vec::new();
-
-  f.read_to_end(&mut op_code).expect("Error while reading file.");
-
-  let chip8 = Chip8::new(op_code);
-  for byte in chip8.memory.iter().enumerate() {
-    println!("{:?}",byte);
-  }
+  let mut chip8 = Chip8::new(rom);
+  chip8.run_cycle();
 }
 
-fn decode (op_code: Vec<u8>) {
-    
-}
