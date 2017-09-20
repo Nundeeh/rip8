c0c1f1adc5a80475263309002e970c5a8487f852
raskad <adian11@web.de>
2017-09-20 17:00:55 +0000

    fixed 8XY6

--- src/chip8.rs
+++ src/chip8.rs
@@ -168,8 +168,8 @@ impl Chip8 {
             }
             
             0x0006 => {
-                //8XY5: set V[F] to LSB of V[Y], set V[X] = (V[Y] >> 1)
-                self.register[15] = self.register[((self.opcode & 0x00F0) >> 4) as usize] & 0x000F;
+                //8XY6: set V[F] to LSB of V[Y], set V[X] = (V[Y] >> 1)
+                self.register[15] = self.register[((self.opcode & 0x00F0) >> 4) as usize] & 0x1;
                 self.register[((self.opcode & 0x0F00) >> 8) as usize] = self.register[((self.opcode & 0x00F0) >> 4) as usize] >> 1;
                 self.pc += 2;
             }
